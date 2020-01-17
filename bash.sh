d6cf48dd4425b0b48123ca80db165616bb564a16
stephenblakesanders <blake.sanders32@gmail.com>
2020-01-17 11:25:26 -0500

    bash

---
+++ bash.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "hi"
+echo "list all files"
+ls
+echo "change directory and list files again"
+cd
+ls
+echo "help command"
+help
+echo "play snake"
+snake
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+~                                                                                            
+                                                                           11,5          All
+
